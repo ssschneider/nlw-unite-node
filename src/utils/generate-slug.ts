9fcee649080f3e057a311603a18b553f83edd479
Sarah Schneider <mureb_rosa@hotmail.com>
2024-04-06 13:13:03 -0300

    fix generate slug

--- src/utils/generate-slug.ts
+++ src/utils/generate-slug.ts
@@ -1,7 +1,7 @@
 export function generateSlug (text: string): string {
     return text
         .normalize("NFD") 
-        .replace(/[\u0300-\u36f]/g, "")
+        .replace(/[\u0300-\u036f]/g, "")
         .toLowerCase()
         .replace(/[^\w\s-]/g, "")
         .replace(/\s+/g, "-");
