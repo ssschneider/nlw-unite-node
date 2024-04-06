59800896df30790f8645fece303d9c527803d0aa
Sarah Schneider <mureb_rosa@hotmail.com>
2024-04-06 16:24:13 -0300

    add checkin url to get attendee badge route and type the response

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -11,7 +11,16 @@ export async function getAttendeeBadge (app: FastifyInstance) {
                 params: z.object({
                     attendeeId: z.coerce.number().int(),
                 }),
-                response: {}
+                response: {
+                    200: z.object({
+                        badge: z.object({
+                            name: z.string(),
+                            email: z.string().email(),
+                            eventTitle: z.string(),
+                            checkInURL: z.string().url(),
+                        })
+                    })
+                }
             }
         }, async (request, reply) => {
             const { attendeeId } = request.params;
@@ -35,6 +44,17 @@ export async function getAttendeeBadge (app: FastifyInstance) {
                 throw new Error ("Participante não encontrado");
             };
 
-            reply.send({ attendee });
+            const baseURL = `${request.protocol}://${request.hostname}`;
+
+            const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL);
+
+            reply.send({ 
+                badge: {
+                    name: attendee.name,
+                    email: attendee.email,
+                    eventTitle: attendee.event.title,
+                    checkInURL: checkInURL.toString(),
+                }
+            });
         });
 };
